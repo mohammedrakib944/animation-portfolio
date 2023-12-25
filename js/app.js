4f60ec96b5a611d0c7778a460d98b759a8468c8f
mohammedrakib944 <mohammedrakib944@gmail.com>
2023-12-25 21:56:26 +0600

    button add

--- js/app.js
+++ js/app.js
@@ -11,24 +11,3 @@ function bounce(letter) {
     }, 1000);
   }
 }
-
-// Cursor
-const cursor = document.querySelector(".cursor-dot");
-const cursorOutline = document.querySelector(".cursor-outline");
-const Button = document.querySelector(".button");
-
-document.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.pageX + "px";
-  cursor.style.top = e.pageY + "px";
-
-  cursorOutline.animate(
-    {
-      left: e.pageX + "px",
-      top: e.pageY + "px",
-    },
-    {
-      duration: 500,
-      fill: "forwards",
-    }
-  );
-});

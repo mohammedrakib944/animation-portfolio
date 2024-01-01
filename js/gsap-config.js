242ca122631872b73f1e65cd7ad3cb2edcd42321
mohammedrakib944 <mohammedrakib944@gmail.com>
2024-01-01 11:06:20 +0600

    layout change

--- js/gsap-config.js
+++ js/gsap-config.js
@@ -1,8 +1,9 @@
+gsap.registerPlugin(ScrollTrigger);
 let t1 = gsap.timeline({
   scrollTrigger: {
     trigger: "#skillJs",
     start: "30% 80%", // when we start the animation
-    end: "50% 50%", // when we end the animation
+    end: "50% 70%", // when we end the animation
     scrub: 1, // smoothness of the animation
     markers: true,
     // pin: true,
@@ -14,4 +15,27 @@ t1.to("#skillJs", {
   // x: 200,
   opacity: 1,
   scale: 1,
+});
+
+gsap.to("#profile", {
+  scale: 1,
+  opacity: 1,
+});
+
+gsap.to("#bounce_text", {
+  scale: 1,
+  opacity: 1,
+  delay: 0.3,
+});
+
+gsap.to("#about", {
+  scale: 1,
+  opacity: 1,
+  delay: 0.6,
+});
+
+gsap.to("#hero_links", {
+  scale: 1,
+  opacity: 1,
+  delay: 0.9,
 });

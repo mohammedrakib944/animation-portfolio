5d04ec096a927f9a7f8951b57cabd388c7f8ee4e
mohammedrakib944 <mohammedrakib944@gmail.com>
2024-01-01 12:14:53 +0600

    layout change

--- js/gsap-config.js
+++ js/gsap-config.js
@@ -1,19 +1,19 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("#skillJs", {
-  // x: 200,
-  opacity: 1,
-  scale: 1,
-  scrollTrigger: {
-    trigger: "#skillJs",
-    start: "30% 80%", // when we start the animation
-    end: "50% 70%", // when we end the animation
-    scrub: 1, // smoothness of the animation
-    markers: true,
-    // pin: true,
-    // pinSpacing: false,
-  },
-});
+// gsap.to("#skillJs", {
+//   y: 0,
+//   opacity: 1,
+//   scale: 1,
+//   scrollTrigger: {
+//     trigger: "#skillJs",
+//     start: "30% 80%", // when we start the animation
+//     end: "50% 70%", // when we end the animation
+//     scrub: 1, // smoothness of the animation
+//     markers: true,
+//     // pin: true,
+//     // pinSpacing: false,
+//   },
+// });
 
 gsap.to("#profile", {
   scale: 1,
